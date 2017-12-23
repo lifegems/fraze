f1b7cd03a0d3e1e26aeedfed376091f076726275
Michalik, Joshua <smileyfacepancakes@gmail.com>
2017-12-22 19:16:42 -0500

    Add base pages

--- src/pages/home/home.ts
+++ src/pages/home/home.ts
@@ -6,9 +6,27 @@ import { NavController } from 'ionic-angular';
   templateUrl: 'home.html'
 })
 export class HomePage {
+  public decks = [
+    {
+      title: "Haitian Creole",
+      description: "Pawol yo pou koprann."
+    },
+    {
+      title: "Programming Terms",
+      description: "Abstract meanings within an abstract environment."
+    },
+    {
+      title: "Bible Books",
+      description: "Not including the apocrypha or pseudepograph."
+    },
+  ];
 
   constructor(public navCtrl: NavController) {
 
+  }
+
+  test() {
+    alert("test");
   }
 
 }
