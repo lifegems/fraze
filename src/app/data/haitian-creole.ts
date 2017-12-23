557e4079a56adba5ac96ad3ec3d8de8299329996
TSephanya Brun <tsephanyabrun@TSephanyas-MacBook-Pro.local>
2017-12-22 21:33:38 -0500

    Add haitian creole deck

---
+++ src/app/data/haitian-creole.ts
@@ -0,0 +1,194 @@
+export const Nimewo = [
+    {
+        sideA: "0",
+        sideB: "zewo"
+    },
+    {
+        sideA: "1",
+        sideB: "en"
+    },
+    {
+        sideA: "2",
+        sideB: "de"
+    },
+    {
+        sideA: "3",
+        sideB: "twa"
+    },
+    {
+        sideA: "4",
+        sideB: "kat"
+    },
+    {
+        sideA: "5",
+        sideB: "senk"
+    },
+    {
+        sideA: "6",
+        sideB: "sis"
+    },
+    {
+        sideA: "7",
+        sideB: "set"
+    },
+    {
+        sideA: "8",
+        sideB: "uit"
+    },
+    {
+        sideA: "9",
+        sideB: "nef"
+    },
+    {
+        sideA: "10",
+        sideB: "dis"
+    },
+    {
+        sideA: "11",
+        sideB: "onz"
+    },
+    {
+        sideA: "12",
+        sideB: "douz"
+    },
+    {
+        sideA: "13",
+        sideB: "trez"
+    },
+    {
+        sideA: "14",
+        sideB: "katoz"
+    },
+    {
+        sideA: "15",
+        sideB: "kenz"
+    },
+    {
+        sideA: "16",
+        sideB: "sez"
+    },
+    {
+        sideA: "17",
+        sideB: "diset"
+    },
+    {
+        sideA: "18",
+        sideB: "dizuit"
+    },
+    {
+        sideA: "19",
+        sideB: "diznef"
+    },
+    {
+        sideA: "20",
+        sideB: "ven"
+    },
+    {
+        sideA: "21",
+        sideB: "venteyen"
+    },
+    {
+        sideA: "22",
+        sideB: "vennde"
+    },
+    {
+        sideA: "23",
+        sideB: "venntwa"
+    },
+    {
+        sideA: "24",
+        sideB: "vennkat"
+    },
+    {
+        sideA: "25",
+        sideB: "vennsenk"
+    },
+    {
+        sideA: "30",
+        sideB: "trant"
+    },
+    {
+        sideA: "40",
+        sideB: "karant"
+    },
+    {
+        sideA: "50",
+        sideB: "senkant"
+    },
+    {
+        sideA: "60",
+        sideB: "swasant"
+    },
+    {
+        sideA: "70",
+        sideB: "swasanndis"
+    },
+    {
+        sideA: "80",
+        sideB: "katreven"
+    },
+    {
+        sideA: "90",
+        sideB: "katrevendis"
+    },
+    {
+        sideA: "100",
+        sideB: "san"
+    },
+    {
+        sideA: "101",
+        sideB: "san en"
+    },
+    {
+        sideA: "200",
+        sideB: "de san"
+    },
+    {
+        sideA: "1 000",
+        sideB: "mil"
+    },
+    {
+        sideA: "1 100",
+        sideB: "mil san"
+    },
+    {
+        sideA: "1 200",
+        sideB: "mil de san"
+    },
+    {
+        sideA: "1 201",
+        sideB: "mil de san en"
+    },
+    {
+        sideA: "2 000",
+        sideB: "de mil"
+    },
+    {
+        sideA: "10 000",
+        sideB: "di mil"
+    },
+    {
+        sideA: "100 000",
+        sideB: "san mil"
+    },
+    {
+        sideA: "1 000 000",
+        sideB: "yon milyon"
+    },
+    {
+        sideA: "2 000 000",
+        sideB: "de milyon"
+    },
+    {
+        sideA: "10 000 000",
+        sideB: "di milyon"
+    },
+    {
+        sideA: "100 000 000",
+        sideB: "san milyon"
+    },
+    {
+        sideA: "1 000 000 000",
+        sideB: "yon bilyon"
+    },
+];
